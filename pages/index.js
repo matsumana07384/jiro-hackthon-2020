3bab57eff749b3fde609515a5b3ece72e786c13b
matsumana07384 <manami.m.227@outlook.jp>
2020-06-06 21:12:52 +0900

    add speak

--- pages/index.js
+++ pages/index.js
@@ -40,9 +40,17 @@ export default function Home() {
               </p>
               <p className="text">@online</p>
             </div>
-            <p className="status">\ 開催中 /</p>
-            <div className="button">
-              <Button variant="contained" size="large" href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">ハッカソンに参加する</Button>
+            <div>
+              <p className="status">\ 参加者の方へ /</p>
+              <div className="button">
+                <Button variant="contained" size="large" href="https://docs.google.com/forms/d/e/1FAIpQLScZDF5aXboythEYrSKcjAPuQxRVLiRESdPUjxHZH1Lv4X62kw/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">発表会へエントリーする</Button>
+              </div>
+            </div>
+            <div>
+              <p className="status">\ 開催中 /</p>
+              <div className="button">
+                <Button variant="contained" size="large" href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">ハッカソンに参加する</Button>
+              </div>
             </div>
           </div>
         </div>
@@ -85,37 +93,54 @@ export default function Home() {
             <p className="title">
               ルール
             </p>
-            <ul className="list">
-              <li>
-              今回のハッカソンはオンライン上で行います。参加に際しては本イベントページでの録を推奨いたしますが、期間が長いため、必須とはしないものとします。基本的には参加者へのDMにて情報をお送りしますが、Twitterのハッシュタグ「 <HashTag />」や <a href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">イベントページ</a>等でも情報を発信し、参加登録されていない方でも確認いただけるよう努めてまいります。
-              </li>
-              <li>開発期間は6/5（金）20:00から6/7（日）17:00の45時間とします。この時間には発表用の資料制作の時間を含みます。期間中でご都合の良い時に開発いただければ構いません。また各自で適切に睡眠を取ってください。スケジュールについては<a href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">イベントページ</a>を合わせてご覧ください。
-              </li>
-              <li>
-              希望される方のチーム編成については、各々にてお願いいたします。その際はハッシュタグ「 <HashTag /> 」をご活用いただければと思います。
-              </li>
-              <li>
-              開発中も、随時Twitterのハッシュタグ「 <HashTag /> 」をご活用いただき、進捗報告やお昼ご飯の共有などでいただき盛り上げていただけますと助かります。
-              </li>
-              <li>
-                発表会は6/7（日）17:00から、ビデオ会議ツール「zoom.us」をもちいて行うものとします。また、作品の状況の確認のため、同日12:00-15:00にエントリーの時間を設けます。また、開発いただいた上で発表会の時間にご予定があるなど、発表会にご参加いただけない方は、資料をご用意いただければ代理にて作品のご紹介をさせていただきます。詳細については6日（土）中に改めてご案内いたします。
-              </li>
-              <li>
-                7日（日）にはYouTube Liveを用いた生放送を予定しております。また、作品の発表会や審査結果発表も同放送内で行う予定です。詳細については7日（日）に改めてご案内いたします。
-              </li>
-              <li>
-                イベント規模が小さいことから、審査については部門ごとの賞などは設けず、総合的な審査を行い、最優秀の作品を1作品と、入賞を数作品選出する予定です。審査基準については、「アイデアの内容」と「使用している技術」の両側面を考慮するよう努めますが、テーマの性質上、「アイデアの内容」、特に「面白さ/斬新さ」の面の比重を大きくした審査となります。
-              </li>
-              <li>
-                優勝された方には個別にご連絡をし、優勝商品の「ニンニク1年分」を手配いたします。原則としてお受け取りください。
-              </li>
-              <li>
-              当日の状況にもよりますが、生放送終了後にご希望の方でzoomを用いたオンライン懇親会を行いたいと考えています。ぜひご参加いただけますと嬉しいです。
-              </li>
-            </ul> 
+            <div className="description">
+              <ul className="list">
+                <li>
+                今回のハッカソンはオンライン上で行います。参加に際しては本イベントページでの録を推奨いたしますが、期間が長いため、必須とはしないものとします。基本的には参加者へのDMにて情報をお送りしますが、Twitterのハッシュタグ「 <HashTag />」や <a href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">イベントページ</a>等でも情報を発信し、参加登録されていない方でも確認いただけるよう努めてまいります。
+                </li>
+                <li>開発期間は6/5（金）20:00から6/7（日）17:00の45時間とします。この時間には発表用の資料制作の時間を含みます。期間中でご都合の良い時に開発いただければ構いません。また各自で適切に睡眠を取ってください。スケジュールについては<a href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">イベントページ</a>を合わせてご覧ください。
+                </li>
+                <li>
+                希望される方のチーム編成については、各々にてお願いいたします。その際はハッシュタグ「 <HashTag /> 」をご活用いただければと思います。
+                </li>
+                <li>
+                開発中も、随時Twitterのハッシュタグ「 <HashTag /> 」をご活用いただき、進捗報告やお昼ご飯の共有などでいただき盛り上げていただけますと助かります。
+                </li>
+                <li>
+                  発表会は6/7（日）17:00から、ビデオ会議ツール「<a href="https://zoom.us/" target="_blank" rel="noopener noreferrer">zoom</a>」をもちいて行うものとします。また、作品の状況の確認のため、同日12:00-15:00にエントリーの時間を設けます。また、開発いただいた上で発表会の時間にご予定があるなど、発表会にご参加いただけない方は、資料をご用意いただければ代理にて作品のご紹介をさせていただきます。詳細については6日（土）中に改めてご案内いたします。
+                </li>
+                <li>
+                  7日（日）にはYouTube Liveを用いた生放送を予定しております。また、作品の発表会や審査結果発表も同放送内で行う予定です。詳細については7日（日）に改めてご案内いたします。
+                </li>
+                <li>
+                  イベント規模が小さいことから、審査については部門ごとの賞などは設けず、総合的な審査を行い、最優秀の作品を1作品と、入賞を数作品選出する予定です。審査基準については、「アイデアの内容」と「使用している技術」の両側面を考慮するよう努めますが、テーマの性質上、「アイデアの内容」、特に「面白さ/斬新さ」の面の比重を大きくした審査となります。
+                </li>
+                <li>
+                  優勝された方には個別にご連絡をし、優勝商品の「ニンニク1年分」を手配いたします。原則としてお受け取りください。
+                </li>
+                <li>
+                当日の状況にもよりますが、生放送終了後にご希望の方でzoomを用いたオンライン懇親会を行いたいと考えています。ぜひご参加いただけますと嬉しいです。
+                </li>
+              </ul> 
+            </div>
+          </div>
+          <div className="section white" id="speak">
+            <p className="title">
+              発表会参加申し込み
+            </p>
+            <div className="button">
+                <Button variant="contained" size="large" href="https://gonativecode.connpass.com/event/173348/" target="_blank" rel="noopener noreferrer">ハッカソンに参加する</Button>
+              </div>
+            <div className="description">
+              <ul className="list">
+                <li>発表会の参加申し込みフォームを開設いたしました。</li>
+                <li>発表会にご参加されない方で資料を共有いただき発表を代行する場合もこちらよりお申込みいただけます。詳細はフォームにご案内しておりますのでご確認ください。</li>
+                <li>期限は2020/06/07（日）15時までといたします。</li>
+              </ul>
+            </div>
           </div>
 
-          {/* <div className="section black" id="schedule">
+          {/* <div className="section white" id="schedule">
             <p className="title">
               参加者スケジュール
             </p>
@@ -164,7 +189,7 @@ export default function Home() {
               </table>
             </div>
           </div>
-          <div className="section white" id="live">
+          <div className="section block" id="live">
             <p className="title">
               生配信スケジュール
             </p>
@@ -199,6 +224,7 @@ export default function Home() {
               </table>
             </p>
           </div> */}
+
         </div>
       </main>
 
@@ -296,6 +322,7 @@ export default function Home() {
           background-image: url("/ramen_bk.jpg");
         }
         .section {
+          scroll-padding-top: 60px;
           margin: 0;
         }
         .black {
@@ -324,9 +351,7 @@ export default function Home() {
         }    
         .list {
           text-align: left;
-          display: inline-block;
-          padding: 0 4rem;
-        }
+          display: inline-block;        }
         .list >li {
           margin: 1rem;
         }
@@ -341,7 +366,7 @@ export default function Home() {
         }
         .status {
           color: #C6161E;
-          font-size: 1rem;
+          font-size: 1.5rem;
           margin-top: 1rem;
         }
 
@@ -350,6 +375,7 @@ export default function Home() {
       <style jsx global>{`
         html,
         body {
+          scroll-padding-top: 60px;
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
